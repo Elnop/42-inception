fcde05842430a9d98adf9e3cc815b5b8771bfa6c
Elnop <elnop@dev.fr>
2024-03-20 14:05:42 +0100

    MariaDB working now

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -1,17 +1,64 @@
-#!/bin/bash
+#!/bin/sh
+# set -eux
 
-service mariadb start
+if [ -d "/run/mysqld" ]; then
+	echo "Setup.sh : mysqld already present, skipping creation"
+	chown -R mysql:mysql /run/mysqld
+else
+	echo "Setup.sh : mysqld not found, creating...."
+	mkdir -p /run/mysqld
+fi
 
-sleep 5
+if [ -d /var/lib/mysql/mysql ]; then
+	echo "Setup.sh : MySQL directory already present, skipping creation"
+else
+	echo "Setup.sh : MySQL data directory not found, creating initial DBs"
+	chmod 700 /var/lib/mysql
+	mysql_install_db --user=root --no-defaults --datadir=/var/lib/mysql > /dev/null
 
-echo "CREATE DATABASE IF NOT EXISTS mymaria;" > db1.sql
-echo "CREATE USER IF NOT EXISTS'$SQL_USER'@'localhost' identified by '$SQL_PASSWORD';" >> db1.sql
-echo "GRANT ALL on mymaria.* to '$SQL_USER'@'localhost';" >> db1.sql
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$SQL_ROOT_PASSWORD' ;" >> db1.sql
-echo "FLUSH PRIVILEGES;" >> db1.sql
+	if [ "$MYSQL_ROOT_PASSWORD" = "" ]; then
+		MYSQL_ROOT_PASSWORD=`pwgen 16 1`
+		echo "Setup.sh : MySQL root Password: $MYSQL_ROOT_PASSWORD"
+	fi
 
-mysql < db1.sql
+	MYSQL_DATABASE=${MYSQL_DATABASE:-""}
+	MYSQL_USER=${MYSQL_USER:-""}
+	MYSQL_PASSWORD=${MYSQL_PASSWORD:-""}
 
-kill $(cat /run/mysqld/mysqld.pid)
+	tfile=`mktemp`
+	if [ ! -f "$tfile" ]; then
+	    return 1
+	fi
 
-mysqld
+	cat << EOF > $tfile
+USE mysql;
+FLUSH PRIVILEGES ;
+GRANT ALL ON *.* TO 'root'@'%' identified by '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION ;
+GRANT ALL ON *.* TO 'root'@'localhost' identified by '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION ;
+SET PASSWORD FOR 'root'@'localhost'=PASSWORD('${MYSQL_ROOT_PASSWORD}') ;
+DROP DATABASE IF EXISTS test ;
+FLUSH PRIVILEGES ;
+EOF
+    if [ "$MYSQL_DATABASE" != "" ]; then
+	    echo "Setup.sh : Creating database: $MYSQL_DATABASE"
+		if [ "$MYSQL_CHARSET" != "" ] && [ "$MYSQL_COLLATION" != "" ]; then
+			echo "Setup.sh : $MYSQL_DATABASE : character set [$MYSQL_CHARSET] | collation [$MYSQL_COLLATION]"
+			echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` CHARACTER SET $MYSQL_CHARSET COLLATE $MYSQL_COLLATION;" >> $tfile
+		else
+			echo "Setup.sh : with character set: 'utf8' and collation: 'utf8_general_ci'"
+			echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` CHARACTER SET utf8 COLLATE utf8_general_ci;" >> $tfile
+		fi
+
+		if [ "$MYSQL_USER" != "" ]; then
+			echo "Setup.sh : Creating user: $MYSQL_USER with password $MYSQL_PASSWORD"
+			echo "GRANT ALL ON \`$MYSQL_DATABASE\`.* to '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> $tfile
+		fi
+	fi
+
+	/usr/bin/mysqld --user=root --bootstrap --verbose=0 --skip-name-resolve --skip-networking=0 < $tfile
+	rm -f $tfile
+    echo 'MySQL init process done. Ready for start up.'
+fi
+/usr/bin/mysqld --user=root --console --skip-name-resolve --skip-networking=0
+echo "--- ERROR LOG ---"
+cat /var/log/mysql/error.log
