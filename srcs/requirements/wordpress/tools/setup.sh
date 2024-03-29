e5bd01c11642c8ae6316ae03d54e0d561e3f399a
Elnop <elnop@dev.fr>
2024-03-29 16:08:52 +0100

    wordpress setup wip

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -1,12 +1,15 @@
-if [! -f /var/www/html/wp-config.php]; then
+#!/bin/sh
+set -eux
+
+if [ ! -f /var/www/html/wp-config.php ]; then
 	curl -o /tmp/wordpress.tar.gz -SL https://wordpress.org/latest.tar.gz
 	tar -xzf /tmp/wordpress.tar.gz -C /var/www/html --strip-components=1
 	rm /tmp/wordpress.tar.gz
-	echo \
-$'\n'"define('DB_NAME', '${WORDPRESS_DB_NAME})'"; \
-$'\n'"define('DB_USER', '${WORDPRESS_DB_USER})'"; \
-$'\n'"define('DB_PASSWORD', '${WORDPRESS_DB_PASSWORD})'"; \
-$'\n'"define('DB_HOST', '${WORDPRESS_DB_HOST})'"; \
+	echo "
+define('DB_NAME', '${WORDPRESS_DB_NAME})'; 
+define('DB_USER', '${WORDPRESS_DB_USER})'; 
+define('DB_PASSWORD', '${WORDPRESS_DB_PASSWORD})';
+define('DB_HOST', '${WORDPRESS_DB_HOST})';"
 >> wp-config.php
 fi
 
